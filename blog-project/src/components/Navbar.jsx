90e56b97ea9a100b4ad0b64be77cbfaa6fd4ea4a
Erik Richard <erichard413@outlook.com>
2023-09-26 11:53:37 -0400

    added scroll restoration to navbar

--- blog-project/src/components/Navbar.jsx
+++ blog-project/src/components/Navbar.jsx
@@ -1,21 +1,24 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, ScrollRestoration } from "react-router-dom";
 
 export function Navbar() {
   return (
-    <nav className="top-nav">
-      <div className="nav-text-large">My App</div>
-      <ul className="nav-list">
-        <li>
-          <Link to="/posts">Posts</Link>
-        </li>
-        <li>
-          <Link to="/users">Users</Link>
-        </li>
-        <li>
-          <Link to="/todos">Todos</Link>
-        </li>
-      </ul>
-    </nav>
+    <>
+      <nav className="top-nav">
+        <div className="nav-text-large">My App</div>
+        <ul className="nav-list">
+          <li>
+            <Link to="/posts">Posts</Link>
+          </li>
+          <li>
+            <Link to="/users">Users</Link>
+          </li>
+          <li>
+            <Link to="/todos">Todos</Link>
+          </li>
+        </ul>
+      </nav>
+      <ScrollRestoration />
+    </>
   );
 }
